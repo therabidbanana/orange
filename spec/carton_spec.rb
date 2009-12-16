da94ced43dfa9058441d6e6c931b63651944813c
David Haslem <therabidbanana@king.local>
2009-12-15 21:51:37 -0500

    Added two spec stubs

---
+++ spec/carton_spec.rb
@@ -0,0 +1,5 @@
+describe Orange::Carton do
+  it "should be spec'ed" do
+    violated "needs to be spec'ed"
+  end
+end
---
+++ spec/magick_spec.rb
@@ -0,0 +1,5 @@
+describe "black magick" do
+  it "should be spec'ed" do
+    violated "needs to be spec'ed"
+  end
+end
