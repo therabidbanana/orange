4d2cf520885fcf79d8533bf4e9d323b1bcb662ac
David Haslem <therabidbanana@gmail.com>
2010-02-20 15:25:43 -0500

    Support for plugins

--- lib/orange-core.rb
+++ lib/orange-core.rb
@@ -1,7 +1,7 @@
 libdir = File.dirname(__FILE__)
 $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 
-require File.join(libdir, 'orange-core', 'magick.rb')
-Dir.glob(File.join(libdir, 'orange-core', '*.rb')).each {|f| require f }
-Dir.glob(File.join(libdir, 'orange-core', 'resources', '*.rb')).each {|f| require f }
-Dir.glob(File.join(libdir, 'orange-core', 'middleware', '*.rb')).each {|f| require f }
+require File.join('orange-core', 'magick.rb')
+Dir.glob(File.join('orange-core', '*.rb')).each {|f| require f }
+Dir.glob(File.join('orange-core', 'resources', '*.rb')).each {|f| require f }
+Dir.glob(File.join('orange-core', 'middleware', '*.rb')).each {|f| require f }
--- lib/orange-more.rb
+++ lib/orange-more.rb
@@ -1,11 +1,11 @@
 libdir = File.dirname(__FILE__)
 $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 
-require File.join(libdir,'orange-core')
+require 'orange-core'
 
-require File.join(libdir, 'orange-core', 'magick.rb')
-Dir.glob(File.join(libdir, 'orange-core', '*.rb')).each {|f| require f }
-Dir.glob(File.join(libdir, 'orange-core', 'cartons', '*.rb')).each {|f| require f }
-require File.join(libdir, 'orange-core', 'resources', 'model_resource.rb')
-Dir.glob(File.join(libdir, 'orange-core', 'resources', '*.rb')).each {|f| require f }
-Dir.glob(File.join(libdir, 'orange-core', 'middleware', '*.rb')).each {|f| require f }
+require File.join('orange-more', 'template')
+require File.join('orange-more', 'administration')
+require File.join('orange-more', 'assets')
+require File.join('orange-more', 'pages')
+require File.join('orange-more', 'sitemap')
+require File.join('orange-more', 'slices')
