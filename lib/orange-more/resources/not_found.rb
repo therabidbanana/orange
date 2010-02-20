0908a68b4f8c2f7154b12979fa82fcc035df0356
David Haslem <therabidbanana@gmail.com>
2010-02-20 15:16:50 -0500

    Getting even more drastic

--- lib/orange-more/resources/not_found.rb
+++
@@ -1,9 +0,0 @@
-module Orange
-  class NotFound < Orange::Resource
-    call_me :not_found
-    def route(packet)
-      packet[:content] = orange[:parser].haml("404.haml", packet, :resource => self)
-      packet[:status] = 404
-    end
-  end
-end
