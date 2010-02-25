fb3c416270822f229a2f1f8b4b82453d34460283
David Haslem <therabidbanana@gmail.com>
2010-02-24 20:10:24 -0500

    Allow a debug url for forcing DB migration

--- lib/orange-core/middleware/database.rb
+++ lib/orange-core/middleware/database.rb
@@ -2,13 +2,20 @@ require 'orange-core/middleware/base'
 module Orange::Middleware
   
   class Database < Base
-    def init(*args)
+    def init(opts = {})
+      opts = opts.with_defaults(:migration_url => (orange.options[:development_mode] ? '/__ORANGE_DB__/migrate' : false))
       orange.mixin Orange::Mixins::DBLoader
       db = orange.options['database'] || 'sqlite3::memory:'
       orange.load_db!(db)
       orange.upgrade_db!
+      @options = opts
     end
     def packet_call(packet)
+      path = packet['route.path'] || packet.request.path_info
+      if @options[:migration_url] && @options[:migration_url] == path
+        orange.migrate_db!
+        packet.reroute('/')
+      end
       pass packet
     end
   end
