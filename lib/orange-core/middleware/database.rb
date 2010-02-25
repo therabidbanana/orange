d32ce8a77285c283bcc62b5d181455752d4e9b7d
David Haslem <therabidbanana@gmail.com>
2010-02-24 19:56:11 -0500

    Rearranged database middleware so it doesn't keep reinitializing every packet

--- lib/orange-core/middleware/database.rb
+++ lib/orange-core/middleware/database.rb
@@ -4,10 +4,11 @@ module Orange::Middleware
   class Database < Base
     def init(*args)
       orange.mixin Orange::Mixins::DBLoader
+      db = orange.options['database'] || 'sqlite3::memory:'
+      orange.load_db!(db)
+      orange.upgrade_db!
     end
     def packet_call(packet)
-      db = packet['orange.globals']['database'] || 'sqlite3::memory:'
-      orange.load_db!(db)
       pass packet
     end
   end
@@ -17,6 +18,14 @@ end
 module Orange::Mixins::DBLoader
   def load_db!(url)
     DataMapper.setup(:default, url)
+  end
+  
+  def migrate_db!
+    DataMapper.auto_migrate!
+  end
+  
+  def upgrade_db!
     DataMapper.auto_upgrade!
   end
+  
 end
