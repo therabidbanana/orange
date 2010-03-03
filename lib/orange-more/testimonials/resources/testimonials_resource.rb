d995358bc0e4187ee9169aa93a61194bd34d9188
Tommy Creo <tom@The-Fountain.(none)>
2010-03-03 18:56:44 -0500

    Fixed testimonials to not crash when there are 0

--- lib/orange-more/testimonials/resources/testimonials_resource.rb
+++ lib/orange-more/testimonials/resources/testimonials_resource.rb
@@ -6,18 +6,21 @@ module Orange
       orange[:admin, true].add_link("Content", :resource => @my_orange_name, :text => 'Testimonials')
       orange.register(:stack_loaded) do
         orange[:radius].context.define_tag "testimonials" do |tag|
-	     	if tag.attr["tag"]
-          m = model_class.with_tag(tag.attr["tag"]).first(:offset => rand(model_class.with_tag(tag.attr["tag"]).count)) #selects testimonial based on tag
-		    else
-        	m = model_class.first(:offset => rand(model_class.all.count)) #selects a random testimonial
-		    end
-		    orange[:testimonials].testimonial(tag.locals.packet, {:model => m })
+	     	  if tag.attr["tag"] && model_class.all.count >0
+            m = model_class.with_tag(tag.attr["tag"]).first(:offset => rand(model_class.with_tag(tag.attr["tag"]).count)) #selects testimonial based on tag
+		      elsif model_class.all.count > 0
+        	  m = model_class.first(:offset => rand(model_class.all.count)) #selects a random testimonial
+		      end
+          unless m.nil?
+            orange[:testimonials].testimonial(tag.locals.packet, {:model => m })
+          else
+            ""
+          end
         end
-     end
+      end
     end
     def testimonial(packet, opts = {})
       do_view(packet, :testimonials, opts)
     end
-    
   end
 end
