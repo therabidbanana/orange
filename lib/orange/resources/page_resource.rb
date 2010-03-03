aa8d5ebb008e8409cd37c34149b3526e93599ea5
Meaghan Kennedy <meaghankennedy@meaghan-kennedys-imac.local>
2010-03-03 12:36:19 -0500

    Ch-ch-ch-chaaaanges

--- lib/orange/resources/page_resource.rb
+++ lib/orange/resources/page_resource.rb
@@ -1,7 +1,24 @@
 module Orange
   module Pulp::PageHelpers
-    def fuzzy_time(t)
-      "foo"
+    def fuzzy_time(from_time)
+      to_time = Time.new
+      from_time = from_time.to_time if from_time.respond_to?(:to_time)
+      to_time = to_time.to_time if to_time.respond_to?(:to_time)
+      distance_in_minutes = (((to_time - from_time).abs)/60).round
+      distance_in_seconds = ((to_time - from_time).abs).round
+        case distance_in_minutes
+          when 0..1
+            return distance_in_minutes == 0 ? "Less than 1 minute ago" : "About 1 minute ago"
+          when 2..44           then "#{distance_in_minutes} minutes ago"
+          when 45..89          then "An hour ago"
+          when 90..1439        then "#{(distance_in_minutes.to_f / 60.0).round} hours ago"
+          when 1440..2879      then "Yesterday"
+          when 2880..43199     then "#{(distance_in_minutes / 1440).round} days ago"
+          when 43200..86399    then "1 Month ago"
+          when 86400..525599   then "#{(distance_in_minutes / 43200).round} months ago"
+          when 525600..1051199 then "1 year ago"
+          else                      "Over #{(distance_in_minutes / 525600).round} years ago"
+        end
     end
   end
   class PageResource < Orange::ModelResource
@@ -37,6 +54,8 @@ module Orange
       end
       packet.reroute(@my_orange_name, :orange)
     end
+    
+    
     
     # Returns a single object found by the model class, given an id. 
     # If id isn't given, we return false.
