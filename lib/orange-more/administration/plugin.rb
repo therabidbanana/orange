0f7b7bd852eb52654edb8ae80d3180752e801eb6
David Haslem <therabidbanana@gmail.com>
2010-03-09 15:49:50 -0500

    Fix passenger load order

--- lib/orange-more/administration/plugin.rb
+++ lib/orange-more/administration/plugin.rb
@@ -1,3 +1,4 @@
+require (File.join(File.dirname(__FILE__), 'cartons', 'site.rb')
 Dir.glob(File.join(File.dirname(__FILE__), 'cartons', '*.rb')).each {|f| require f }
 Dir.glob(File.join(File.dirname(__FILE__), 'resources', '*.rb')).each {|f| require f }
 Dir.glob(File.join(File.dirname(__FILE__), 'middleware', '*.rb')).each {|f| require f }
