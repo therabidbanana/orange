b06916c84ffc55fba7ac210e7755af9fb872b0a7
David Haslem <therabidbanana@gmail.com>
2010-03-12 14:36:27 -0500

    Added ability to raise a 404.

--- lib/orange-core/middleware/four_oh_four.rb
+++ lib/orange-core/middleware/four_oh_four.rb
@@ -14,14 +14,32 @@ module Orange::Middleware
     def init(opts = {})
       @resource = opts[:resource] || Orange::NotFound
       orange.load @resource.new, :not_found
+      orange.add_pulp Orange::Pulp::NotFoundHelper
     end
     
     # Sets the sitemap resource as the router if the resource can accept 
     # the path.
     def packet_call(packet)
       packet['route.router'] = orange[:not_found] unless packet['route.router']
-      pass packet
+      begin
+        pass packet
+      rescue Orange::NotFoundException
+        orange[:not_found].route(packet)
+        packet.finish
+      end
     end
     
+  end
+end
+
+module Orange
+  class NotFoundException < Exception
+    
+  end
+  
+  module Pulp::NotFoundHelper
+    def not_found
+      raise Orange::NotFoundException.new
+    end
   end
 end
