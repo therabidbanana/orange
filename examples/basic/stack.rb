4eb9cb5b27def2f3155094414c5155de44939d1a
David Haslem <therabidbanana@gmail.com>
2010-05-07 15:48:45 -0400

    Updating example a bit.

--- examples/basic/stack.rb
+++ examples/basic/stack.rb
@@ -7,13 +7,13 @@ require 'openid_dm_store'
 
 class Main < Orange::Application
   stack do
-    orange.options[:development_mode] = true
+    # orange.options[:development_mode] = true
     
     use Rack::CommonLogger
     use Rack::MethodOverride
     use Rack::Session::Cookie, :secret => 'orange_secret'
 
-    auto_reload!
+    # auto_reload!
     use_exceptions
     
     use Rack::OpenID, OpenIDDataMapper::DataMapperStore.new
