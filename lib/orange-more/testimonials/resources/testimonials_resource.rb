2c5d15c0995f0ab47fca847dfe3c68e90264df15
David Haslem <therabidbanana@gmail.com>
2010-05-10 11:06:26 -0400

    Fix testimonials randomization

--- lib/orange-more/testimonials/resources/testimonials_resource.rb
+++ lib/orange-more/testimonials/resources/testimonials_resource.rb
@@ -5,7 +5,7 @@ module Orange
     def stack_init
       orange[:admin, true].add_link("Content", :resource => @my_orange_name, :text => 'Testimonials')
       orange[:radius].define_tag "testimonials" do |tag|
-     	  if tag.attr["tag"] && model_class.all.count >0
+     	  if tag.attr["tag"] && model_class.with_tag(tag.attr["tag"]).count >0
           m = model_class.with_tag(tag.attr["tag"]).first(:offset => rand(model_class.with_tag(tag.attr["tag"]).count)) #selects testimonial based on tag
 	      elsif model_class.all.count > 0
       	  m = model_class.first(:offset => rand(model_class.all.count)) #selects a random testimonial
--- orange.gemspec
+++ orange.gemspec
@@ -9,7 +9,7 @@ Gem::Specification.new do |s|
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["David Haslem"]
-  s.date = %q{2010-05-09}
+  s.date = %q{2010-05-10}
   s.description = %q{Orange is a Ruby framework for building managed websites with code as simple as Sinatra}
   s.email = %q{therabidbanana@gmail.com}
   s.extra_rdoc_files = [
