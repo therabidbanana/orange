6cd8a4bb3364b15e5f1ed59e37d3a37556a0aeea
David Haslem <therabidbanana@gmail.com>
2010-05-13 10:53:10 -0400

    Added calendar radius tag

--- lib/orange-more/events/resources/calendar_resource.rb
+++ lib/orange-more/events/resources/calendar_resource.rb
@@ -4,6 +4,21 @@ module Orange
     call_me :calendar
     def stack_init
       orange[:admin, true].add_link("Settings", :resource => @my_orange_name, :text => 'Calendars')
+      orange[:radius].define_tag "calendar" do |tag|
+     	  template = tag.attr["template"] || false
+     	  if tag.attr["name"]
+     	    calendars = tag.attr["name"].split.map{|x| model_class.first(:name => x)}
+        else
+          calendars = model_class.all
+        end
+        events = OrangeEvent.all(:calendar => calendars, :order => [:starts.asc], :limit => 10)
+        orange[:calendar].calendar(tag.locals.packet, {:list => events, :template => template})
+      end
+    end
+    
+    def calendar(packet, opts = {})
+      template = (opts[:template] || "calendar").to_sym
+      do_list_view(packet, template, opts)
     end
     
     def afterNew(packet, obj, opts = {})
