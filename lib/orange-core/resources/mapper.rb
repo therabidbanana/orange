f06c2cb9be43a5266e670acbe3a1c220debbeca8
David Haslem <therabidbanana@gmail.com>
2010-05-15 19:29:28 -0400

    Don't include the context if it's default...

--- lib/orange-core/resources/mapper.rb
+++ lib/orange-core/resources/mapper.rb
@@ -10,7 +10,8 @@ module Orange
     def route_to(packet, resource, *args)
       opts = args.extract_options!
       packet = DefaultHash.new unless packet 
-      context = opts[:context] || packet['route.context', nil]
+      context = opts[:context]
+      context = packet['route.context', nil] unless context || packet['route.context'] == :live
       site = packet['route.faked_site'] ? packet['route.site_url', nil] : nil
       args.unshift(resource)
       args.unshift(context)
